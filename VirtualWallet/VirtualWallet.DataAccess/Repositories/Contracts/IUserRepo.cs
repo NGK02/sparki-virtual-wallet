ec70acc938bd390ca61c1aaabe428575b9720fba
Atanas Iliev <atanasiliev1293@gmail.com>
2023-07-19 16:23:45 +0300

    Added Folders for Services and Repositories Added folders for ApiControllers and ViewControllers

---
+++ VirtualWallet/VirtualWallet.DataAccess/Repositories/Contracts/IUserRepo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualWallet.DataAccess.Repositories.Contracts
+{
+	internal interface IUserRepo
+	{
+	}
+}
