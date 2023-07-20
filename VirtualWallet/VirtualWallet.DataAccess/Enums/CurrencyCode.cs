cef018fea494677030a8546714207e81b2b8c82d
Nikolai Gigov <nikolaivgigov2@gmail.com>
2023-07-20 13:39:46 +0300

    -Added enums RoleName and CurrencyCode -Implemented them into models, seeded and configured converting to string for db

---
+++ VirtualWallet/VirtualWallet.DataAccess/Enums/CurrencyCode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VirtualWallet.DataAccess.Models;
+
+namespace VirtualWallet.DataAccess.Enums
+{
+	public enum CurrencyCode
+	{
+		USD = 1,
+		EUR = 2,
+		BGN = 3,
+		JPY = 4,
+		CFH = 5
+	}
+}
