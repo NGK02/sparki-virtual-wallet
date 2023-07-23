67b64d50343fe4798fc301808d82fde252a55ae8
katrinlilova <katrin.lilova@proton.me>
2023-07-23 19:43:13 +0300

    Finish implementing TransferApiController

--- VirtualWallet/VirtualWalletWeb/ApiControllers/TransferApiController.cs
+++ VirtualWallet/VirtualWalletWeb/ApiControllers/TransferApiController.cs
@@ -61,7 +61,148 @@ namespace VirtualWallet.Web.ApiControllers
                 wallet.Transfers.Add(transfer);
 
                 transferService.AddTransfer(username, transfer);
+
                 return Ok(transfer);
+            }
+            catch (EntityNotFoundException ex)
+            {
+                return StatusCode(StatusCodes.Status404NotFound, ex.Message);
+            }
+            catch (UnauthenticatedOperationException ex)
+            {
+                return StatusCode(StatusCodes.Status401Unauthorized, ex.Message);
+            }
+            catch (UnauthorizedOperationException ex)
+            {
+                return StatusCode(StatusCodes.Status403Forbidden, ex.Message);
+            }
+            catch (ArgumentException ex)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpDelete("{transferId}")]
+        public IActionResult DeleteTransfer([FromHeader] string credentials, int transferId)
+        {
+            try
+            {
+                var splitCredentials = authManager.SplitCredentials(credentials);
+
+                authManager.IsAuthenticated(splitCredentials);
+                string username = splitCredentials[0];
+                transferService.DeleteTransfer(transferId, username);
+
+                return NoContent();
+            }
+            catch (EntityNotFoundException ex)
+            {
+                return StatusCode(StatusCodes.Status404NotFound, ex.Message);
+            }
+            catch (UnauthenticatedOperationException ex)
+            {
+                return StatusCode(StatusCodes.Status401Unauthorized, ex.Message);
+            }
+            catch (UnauthorizedOperationException ex)
+            {
+                return StatusCode(StatusCodes.Status403Forbidden, ex.Message);
+            }
+            catch (ArgumentException ex)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpGet("{transferId}")]
+        public IActionResult GetTransferById([FromHeader] string credentials, int transferId)
+        {
+            try
+            {
+                var splitCredentials = authManager.SplitCredentials(credentials);
+
+                authManager.IsAuthenticated(splitCredentials);
+                string username = splitCredentials[0];
+                var transfer = transferService.GetTransferById(transferId, username);
+
+                return Ok(transfer);
+            }
+            catch (EntityNotFoundException ex)
+            {
+                return StatusCode(StatusCodes.Status404NotFound, ex.Message);
+            }
+            catch (UnauthenticatedOperationException ex)
+            {
+                return StatusCode(StatusCodes.Status401Unauthorized, ex.Message);
+            }
+            catch (UnauthorizedOperationException ex)
+            {
+                return StatusCode(StatusCodes.Status403Forbidden, ex.Message);
+            }
+            catch (ArgumentException ex)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetTransfers([FromHeader] string credentials)
+        {
+            try
+            {
+                var splitCredentials = authManager.SplitCredentials(credentials);
+
+                authManager.IsAuthenticated(splitCredentials);
+                string username = splitCredentials[0];
+                var transfers = transferService.GetTransfers(username);
+
+                return Ok(transfers);
+            }
+            catch (EntityNotFoundException ex)
+            {
+                return StatusCode(StatusCodes.Status404NotFound, ex.Message);
+            }
+            catch (UnauthenticatedOperationException ex)
+            {
+                return StatusCode(StatusCodes.Status401Unauthorized, ex.Message);
+            }
+            catch (UnauthorizedOperationException ex)
+            {
+                return StatusCode(StatusCodes.Status403Forbidden, ex.Message);
+            }
+            catch (ArgumentException ex)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpGet("wallet")]
+        public IActionResult GetWalletTransfers([FromHeader] string credentials)
+        {
+            try
+            {
+                var splitCredentials = authManager.SplitCredentials(credentials);
+
+                authManager.IsAuthenticated(splitCredentials);
+                string username = splitCredentials[0];
+                var transfers = transferService.GetWalletTransfers(username);
+
+                return Ok(transfers);
             }
             catch (EntityNotFoundException ex)
             {
