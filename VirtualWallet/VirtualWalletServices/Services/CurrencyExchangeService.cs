4be08bf44abe496c416457a24109ee42326f2e28
Nikolai Gigov <nikolaivgigov2@gmail.com>
2023-07-23 10:06:38 +0300

    -Created CurrencyExchangeService and implemented an API call to get the conversion rate between 2 currencies -Added Newtonsoft.Json for parsing Json files

---
+++ VirtualWallet/VirtualWalletServices/Services/CurrencyExchangeService.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace VirtualWallet.Business.Services
+{
+	public class CurrencyExchangeService
+	{
+		public async Task<string> GetExchangeRate(string from, string to)
+		{
+			using (var client = new HttpClient())
+			{
+				try
+				{
+					client.BaseAddress = new Uri("https://www.exchangerate-api.com");
+					var response = await client.GetAsync($"https://v6.exchangerate-api.com/v6/0be94dda6c7c1a2c97df4970/pair/{from}/{to}");
+					var stringResult = await response.Content.ReadAsStringAsync();
+					var dictResult = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, string>>>(stringResult);
+					return dictResult[$"{from}_{to}"]["val"];
+				}
+				catch (HttpRequestException httpRequestException)
+				{
+					Console.WriteLine(httpRequestException.StackTrace);
+					return "Error calling API. Please do manual lookup.";
+				}
+			}
+		}
+	}
+}
