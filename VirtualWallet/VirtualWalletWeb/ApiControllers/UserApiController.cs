91d756c8e7fefd04640b159f658d5fe6499b1612
Atanas Iliev <atanasiliev1293@gmail.com>
2023-07-27 13:04:28 +0300

    Configured PUT and GET Exchange actions to return mapped GetExchanedObject

--- VirtualWallet/VirtualWalletWeb/ApiControllers/UserApiController.cs
+++ VirtualWallet/VirtualWalletWeb/ApiControllers/UserApiController.cs
@@ -8,6 +8,7 @@ using VirtualWallet.DataAccess.Models;
 using VirtualWallet.Dto.UserDto;
 using System.Net;
 using VirtualWallet.Business.AuthManager;
+using VirtualWallet.Dto.CreateExcahngeDto;
 using VirtualWallet.Dto.ExchangeDto;
 
 namespace VirtualWallet.Web.ApiControllers
@@ -91,7 +92,7 @@ namespace VirtualWallet.Web.ApiControllers
 		}
 
 		[HttpPut("{id}")]
-		public IActionResult EditUser([FromHeader] string credentials,[FromRoute] int id, [FromBody] UpdateUserDto userValues)
+		public IActionResult EditUser([FromHeader] string credentials, [FromRoute] int id, [FromBody] UpdateUserDto userValues)
 		{
 			try
 			{
@@ -142,7 +143,7 @@ namespace VirtualWallet.Web.ApiControllers
 			try
 			{
 				var splitCredentials = authManager.SplitCredentials(credentials);
-				var loggedUser=authManager.IsAuthenticated(splitCredentials);
+				var loggedUser = authManager.IsAuthenticated(splitCredentials);
 				authManager.IsContentCreatorOrAdmin(loggedUser, id);
 				string username = splitCredentials[0];
 
@@ -169,7 +170,7 @@ namespace VirtualWallet.Web.ApiControllers
 		}
 
 		[HttpPut("{id}/exchanges")]
-		public async Task<IActionResult> ExchangeCurrency([FromHeader] string credentials, [FromRoute] int id, [FromBody] ExcahngeDTO excahngeValues)
+		public async Task<IActionResult> ExchangeCurrency([FromHeader] string credentials, [FromRoute] int id, [FromBody] CreateExcahngeDto excahngeValues)
 		{
 			try
 			{
@@ -178,9 +179,9 @@ namespace VirtualWallet.Web.ApiControllers
 				authManager.IsContentCreatorOrAdmin(loggedUser, id);
 				string username = splitCredentials[0];
 
-				var wallet = await walletService.ExchangeFunds(excahngeValues, loggedUser.WalletId, id);
-
-				return Ok(wallet);
+				var exchange = await walletService.ExchangeFunds(excahngeValues, loggedUser.WalletId, id);
+				var GetExchange = mapper.Map<GetExchangeDto>(exchange);
+				return Ok(GetExchange);
 			}
 			catch (InsufficientFundsException ex)
 			{
@@ -213,7 +214,7 @@ namespace VirtualWallet.Web.ApiControllers
 				var user = authManager.IsAuthenticated(splitCredentials);
 
 				authManager.IsContentCreatorOrAdmin(user, id);
-				var exchanges = exchangeService.GetUserExchanges(id);
+				var exchanges = exchangeService.GetUserExchanges(id).Select(e => mapper.Map<GetExchangeDto>(e));
 
 				return Ok(exchanges);
 			}
