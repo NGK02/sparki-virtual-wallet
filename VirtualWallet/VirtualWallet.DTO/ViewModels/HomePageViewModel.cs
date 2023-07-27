76881439ee62be15884c2f71aaa7f7120effc8f4
Atanas Iliev <atanasiliev1293@gmail.com>
2023-07-27 22:36:41 +0300

    added _ViewImports and _ViewStart also HomePageViewModel

---
+++ VirtualWallet/VirtualWallet.DTO/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualWallet.Dto.ViewModels
+{
+	public class HomePageViewModel
+	{
+		public int usersCount { get; set; }
+
+		public int transactionsCount { get; set; }
+	}
+}
