e6dade472818edd16e3c61ef9dd26463e6492b76
Nikolai Gigov <nikolaivgigov2@gmail.com>
2023-07-27 09:28:37 +0300

    -Added GetTransferDto model

--- VirtualWallet/VirtualWallet.DTO/TransferDto/GetTransferDto.cs
+++ VirtualWallet/VirtualWallet.DTO/TransferDto/GetTransferDto.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace VirtualWallet.Dto.TransferDto
 {
-	internal class GetTransferDto
+	public class GetTransferDto
 	{
+		public int WalletId { get; set; }
+
+		public bool IsCardSender { get; set; }
+
+		public int CardId { get; set; }
+
+		public string CurrencyCode { get; set; }
+
+		public decimal Amount { get; set; }
 	}
 }
