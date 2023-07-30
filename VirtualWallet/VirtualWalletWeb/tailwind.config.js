eb7d64b74946eb74e6bcb480ec84c968892f6e8f
Nikolai Gigov <nikolaivgigov2@gmail.com>
2023-07-30 11:10:25 +0300

    -Added hover effects and transitions to navbar elements

--- VirtualWallet/VirtualWalletWeb/tailwind.config.js
+++ VirtualWallet/VirtualWalletWeb/tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
     darkMode: 'class',
     content: ["./views/**/*.cshtml"],
     theme: {
-    extend: {},
+        //colors: {
+        //    'text': "hex(var(--color-text) / <alpha-value>)",
+        //    'background': "hex(var(--color-background) / <alpha-value>)",
+        //    'primary': "hex(var(--color-primary) / <alpha-value>)",
+        //    'secondary': "hex(var(--color-secondary) / <alpha-value>)",
+        //    'accent': "hex(var(--color-accent) / <alpha-value>)",
+        //},
+
+        extend: {
+        },
   },
   plugins: [],
 }
