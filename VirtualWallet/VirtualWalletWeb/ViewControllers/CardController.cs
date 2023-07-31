e6eb56de45c7efa2a0dcf56a1b44b7055ef70b86
katrinlilova <katrin.lilova@proton.me>
2023-07-31 18:20:25 +0300

    Add get and post 'Edit' actions to CardController and successfully implement both to update resources from the database

--- VirtualWallet/VirtualWalletWeb/ViewControllers/CardController.cs
+++ VirtualWallet/VirtualWalletWeb/ViewControllers/CardController.cs
@@ -27,7 +27,7 @@ namespace VirtualWallet.Web.ViewControllers
         }
 
         [HttpGet]
-        public IActionResult Create()
+        public IActionResult Add()
         {
             if (!authManagerMVC.isLogged("LoggedUser"))
             {
@@ -38,21 +38,26 @@ namespace VirtualWallet.Web.ViewControllers
         }
 
         [HttpPost]
-        public IActionResult Create(CreateCardViewModel createCardViewModel)
+        public IActionResult Add(CreateCardViewModel model)
         {
             try
             {
-                if (!ModelState.IsValid)
+                if (!authManagerMVC.isLogged("LoggedUser"))
                 {
-                    return View(createCardViewModel);
+                    return RedirectToAction("Login", "User");
                 }
 
                 int userId = HttpContext.Session.GetInt32("userId") ?? 0;
 
-                // Consider moving the currency assignment logic to the service layer
-                var currency = currencyService.GetCurrencyByCode(createCardViewModel.CurrencyCode);
+                if (!ModelState.IsValid)
+                {
+                    return View(model);
+                }
 
-                string date = $"{createCardViewModel.ExpirationMonth}-{createCardViewModel.ExpirationYear}";
+                // Consider moving the currency assignment logic to the service layer
+                var currency = currencyService.GetCurrencyByCode(model.CurrencyCode);
+
+                string date = $"{model.ExpirationMonth}-{model.ExpirationYear}";
 
                 // Move the date parsing logic to a separate method or a utility class
                 if (!DateTime.TryParseExact(date, "MM-yyyy", System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out DateTime expirationDate))
@@ -63,15 +68,112 @@ namespace VirtualWallet.Web.ViewControllers
                 // Use AutoMapper
                 var card = new Card
                 {
-                    CardHolder = createCardViewModel.CardHolder,
-                    CardNumber = createCardViewModel.CardNumber,
-                    CheckNumber = createCardViewModel.CheckNumber,
+                    CardHolder = model.CardHolder,
+                    CardNumber = model.CardNumber,
+                    CheckNumber = model.CheckNumber,
                     Currency = currency,
                     CurrencyId = currency.Id,
                     ExpirationDate = expirationDate
                 };
 
                 cardService.AddCard(card, userId);
+                return RedirectToAction("Index", "Home");
+            }
+            catch (EntityNotFoundException ex)
+            {
+                return StatusCode(StatusCodes.Status404NotFound, ex.Message);
+            }
+            catch (UnauthenticatedOperationException ex)
+            {
+                return StatusCode(StatusCodes.Status401Unauthorized, ex.Message);
+            }
+            catch (UnauthorizedOperationException ex)
+            {
+                return StatusCode(StatusCodes.Status403Forbidden, ex.Message);
+            }
+            catch (ArgumentException ex)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpGet("Card/Edit/{cardId}")]
+        public IActionResult Edit(int cardId)
+        {
+            if (!authManagerMVC.isLogged("LoggedUser"))
+            {
+                return RedirectToAction("Login", "User");
+            }
+
+            int userId = HttpContext.Session.GetInt32("userId") ?? 0;
+            var card = cardService.GetCardById(cardId, userId);
+
+            if (!authManagerMVC.isAdmin("roleId") && !authManagerMVC.isContentCreator("userId", card.UserId))
+            {
+                HttpContext.Response.StatusCode = StatusCodes.Status403Forbidden;
+                return View("Error");
+            }
+
+            string month = card.ExpirationDate.ToString("MM");
+            string year = card.ExpirationDate.ToString("yyyy");
+
+            var model = new EditCardViewModel
+            {
+                CardHolder = card.CardHolder,
+                CardNumber = card.CardNumber,
+                CheckNumber = card.CheckNumber,
+                CurrencyCode = card.Currency.Code.ToString(),
+                ExpirationMonth = month,
+                ExpirationYear = year
+            };
+
+            return View(model);
+        }
+
+        [HttpPost("Card/Edit/{cardId}")]
+        public IActionResult Edit(EditCardViewModel model, int cardId)
+        {
+            try
+            {
+                if (!authManagerMVC.isLogged("LoggedUser"))
+                {
+                    return RedirectToAction("Login", "User");
+                }
+
+                int userId = HttpContext.Session.GetInt32("userId") ?? 0;
+
+                if (!ModelState.IsValid)
+                {
+                    return View(model);
+                }
+
+                // Consider moving the currency assignment logic to the service layer
+                var currency = currencyService.GetCurrencyByCode(model.CurrencyCode);
+
+                string date = $"{model.ExpirationMonth}-{model.ExpirationYear}";
+
+                // Move the date parsing logic to a separate method or a utility class
+                if (!DateTime.TryParseExact(date, "MM-yyyy", System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out DateTime expirationDate))
+                {
+                    throw new ArgumentException("Invalid expiration date format.");
+                }
+
+                // Use AutoMapper
+                var card = new Card
+                {
+                    CardHolder = model.CardHolder,
+                    CardNumber = model.CardNumber,
+                    CheckNumber = model.CheckNumber,
+                    Currency = currency,
+                    CurrencyId = currency.Id,
+                    ExpirationDate = expirationDate
+                };
+
+                cardService.UpdateCard(card, cardId, userId);
                 return RedirectToAction("Index", "Home");
             }
             catch (EntityNotFoundException ex)
