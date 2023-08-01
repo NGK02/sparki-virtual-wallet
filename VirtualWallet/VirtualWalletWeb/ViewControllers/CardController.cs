e37eb6ad709252ae38a210ef422e0581ef45dbc9
katrinlilova <katrin.lilova@proton.me>
2023-08-01 16:46:39 +0300

    Implement Delete action in CardController

--- VirtualWallet/VirtualWalletWeb/ViewControllers/CardController.cs
+++ VirtualWallet/VirtualWalletWeb/ViewControllers/CardController.cs
@@ -24,7 +24,7 @@ namespace VirtualWallet.Web.ViewControllers
             this.mapper = mapper;
         }
 
-        [HttpGet]
+        [HttpGet("Card/Add")]
         public IActionResult Add()
         {
             if (!authManagerMVC.isLogged("LoggedUser"))
@@ -35,7 +35,7 @@ namespace VirtualWallet.Web.ViewControllers
             return View();
         }
 
-        [HttpPost]
+        [HttpPost("Card/Add")]
         public IActionResult Add(CardViewModel model)
         {
             try
@@ -55,6 +55,65 @@ namespace VirtualWallet.Web.ViewControllers
                 var card = mapper.Map<Card>(model);
 
                 cardService.AddCard(card, userId);
+                return RedirectToAction("Index", "Home");
+            }
+            catch (EntityNotFoundException ex)
+            {
+                Response.StatusCode = StatusCodes.Status404NotFound;
+                ViewData["ErrorMessage"] = ex.Message;
+
+                return View("Error");
+            }
+            catch (UnauthenticatedOperationException ex)
+            {
+                Response.StatusCode = StatusCodes.Status401Unauthorized;
+                ViewData["ErrorMessage"] = ex.Message;
+
+                return View("Error");
+            }
+            catch (UnauthorizedOperationException ex)
+            {
+                Response.StatusCode = StatusCodes.Status403Forbidden;
+                ViewData["ErrorMessage"] = ex.Message;
+
+                return View("Error");
+            }
+            catch (ArgumentException ex)
+            {
+                Response.StatusCode = StatusCodes.Status400BadRequest;
+                ViewData["ErrorMessage"] = ex.Message;
+
+                return View("Error");
+            }
+            catch (Exception ex)
+            {
+                Response.StatusCode = StatusCodes.Status500InternalServerError;
+                ViewData["ErrorMessage"] = ex.Message;
+
+                return View("Error");
+            }
+        }
+
+        [HttpPost("Card/Delete/{cardId}")]
+        public IActionResult Delete(int cardId)
+        {
+            try
+            {
+                if (!authManagerMVC.isLogged("LoggedUser"))
+                {
+                    return RedirectToAction("Login", "User");
+                }
+
+                int userId = HttpContext.Session.GetInt32("userId") ?? 0;
+                var card = cardService.GetCardById(cardId, userId);
+
+                if (!authManagerMVC.isAdmin("roleId") && !authManagerMVC.isContentCreator("userId", card.UserId))
+                {
+                    Response.StatusCode = StatusCodes.Status403Forbidden;
+                    return View("Error");
+                }
+
+                cardService.DeleteCard(cardId, userId);
                 return RedirectToAction("Index", "Home");
             }
             catch (EntityNotFoundException ex)
