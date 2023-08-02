bb19566c2377d63117ff92a9b00fa823995d3226
Atanas Iliev <atanasiliev1293@gmail.com>
2023-08-02 14:53:43 +0300

    Added user profile picture in navbar

--- VirtualWallet/VirtualWalletWeb/ViewControllers/UserController.cs
+++ VirtualWallet/VirtualWalletWeb/ViewControllers/UserController.cs
@@ -12,240 +12,242 @@ using VirtualWallet.Web.Helper.Contracts;
 
 namespace VirtualWallet.Web.ViewControllers
 {
-	public class UserController : Controller
-	{
-		private readonly IUserService userService;
-		private readonly IMapper mapper;
-		private readonly IAuthManager authManager;
-		private readonly IImageManager imageManager;
-		private readonly IAuthManagerMVC authManagerMVC;
+    public class UserController : Controller
+    {
+        private readonly IUserService userService;
+        private readonly IMapper mapper;
+        private readonly IAuthManager authManager;
+        private readonly IImageManager imageManager;
+        private readonly IAuthManagerMVC authManagerMVC;
 
-		public UserController(IUserService userService,
-								IAuthManager authManager,
-								IMapper mapper,
-								IImageManager imageManager,
-								IAuthManagerMVC authManagerMVC)
-		{
-			this.userService = userService;
-			this.authManager = authManager;
-			this.mapper = mapper;
-			this.imageManager = imageManager;
-			this.authManagerMVC = authManagerMVC;
-		}
-		[HttpGet]
-		public IActionResult Login()
-		{
-			var login = new Login();
-			return View(login);
-		}
+        public UserController(IUserService userService,
+                                IAuthManager authManager,
+                                IMapper mapper,
+                                IImageManager imageManager,
+                                IAuthManagerMVC authManagerMVC)
+        {
+            this.userService = userService;
+            this.authManager = authManager;
+            this.mapper = mapper;
+            this.imageManager = imageManager;
+            this.authManagerMVC = authManagerMVC;
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            var login = new Login();
+            return View(login);
+        }
 
-		[HttpPost]
-		public IActionResult Login(Login filledLoginForm)
-		{
-			//Влизаме тук ако имаме Null Username or Password!
-			if (!this.ModelState.IsValid)
-			{
-				return View(filledLoginForm);
-			}
+        [HttpPost]
+        public IActionResult Login(Login filledLoginForm)
+        {
+            //Влизаме тук ако имаме Null Username or Password!
+            if (!this.ModelState.IsValid)
+            {
+                return View(filledLoginForm);
+            }
 
-			try
-			{
-				var user = authManager.IsAuthenticated(new string[] { filledLoginForm.Username, filledLoginForm.Password });
+            try
+            {
+                var user = authManager.IsAuthenticated(new string[] { filledLoginForm.Username, filledLoginForm.Password });
 
-				this.HttpContext.Session.SetString("LoggedUser", filledLoginForm.Username);
-				this.HttpContext.Session.SetInt32("userId", user.Id);
-				this.HttpContext.Session.SetInt32("roleId", user.RoleId);
+                this.HttpContext.Session.SetString("LoggedUser", filledLoginForm.Username);
+                this.HttpContext.Session.SetInt32("userId", user.Id);
+                this.HttpContext.Session.SetInt32("roleId", user.RoleId);
+                this.HttpContext.Session.SetString("profilePicPath", user.ProfilePicPath ?? "/Assets/pfp.jpg");
 
-				return RedirectToAction("Index", "Home");
+                return RedirectToAction("Index", "Home");
 
-			}
-			catch (EntityNotFoundException)
-			{
-				this.Response.StatusCode = StatusCodes.Status404NotFound;
-				this.ViewData["ErrorMessage"] = "Invalid username or password!";
-				return View(filledLoginForm);
-			}
-			catch (UnauthenticatedOperationException e)
-			{
-				this.Response.StatusCode = StatusCodes.Status403Forbidden;
-				this.ViewData["ErrorMessage"] = e.Message;
-				return View(filledLoginForm);
-			}
-			catch (Exception e)
-			{
-				this.Response.StatusCode = StatusCodes.Status500InternalServerError;
-				this.ViewData["ErrorMessage"] = e.Message;
-				return View("Error");
-			}
-		}
-		[HttpGet]
-		public IActionResult Logout()
-		{
-			this.HttpContext.Session.Remove("LoggedUser");
-			this.HttpContext.Session.Remove("userId");
-			this.HttpContext.Session.Remove("roleId");
-			return RedirectToAction("Index", "Home");
-		}
+            }
+            catch (EntityNotFoundException)
+            {
+                this.Response.StatusCode = StatusCodes.Status404NotFound;
+                this.ViewData["ErrorMessage"] = "Invalid username or password!";
+                return View(filledLoginForm);
+            }
+            catch (UnauthenticatedOperationException e)
+            {
+                this.Response.StatusCode = StatusCodes.Status403Forbidden;
+                this.ViewData["ErrorMessage"] = e.Message;
+                return View(filledLoginForm);
+            }
+            catch (Exception e)
+            {
+                this.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                this.ViewData["ErrorMessage"] = e.Message;
+                return View("Error");
+            }
+        }
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            this.HttpContext.Session.Remove("LoggedUser");
+            this.HttpContext.Session.Remove("userId");
+            this.HttpContext.Session.Remove("roleId");
+            this.HttpContext.Session.Remove("profilePicPath");
+            return RedirectToAction("Index", "Home");
+        }
 
-		[HttpGet]
-		public IActionResult Register()
-		{
-			var registerUser = new RegisterUser();
-			return View(registerUser);
-		}
-		[HttpPost]
-		public IActionResult Register(RegisterUser filledForm)
-		{
-			try
-			{
-				if (!this.ModelState.IsValid)
-				{
-					return View(filledForm);
-				}
+        [HttpGet]
+        public IActionResult Register()
+        {
+            var registerUser = new RegisterUser();
+            return View(registerUser);
+        }
+        [HttpPost]
+        public IActionResult Register(RegisterUser filledForm)
+        {
+            try
+            {
+                if (!this.ModelState.IsValid)
+                {
+                    return View(filledForm);
+                }
 
-				var user = mapper.Map<User>(filledForm);
+                var user = mapper.Map<User>(filledForm);
 
-				if (filledForm.ProfilePic is null)
-				{
-					filledForm.ProfilePic = imageManager.GeneratePlaceholderAvatar(filledForm.FirstName, filledForm.LastName);
-					user.ProfilePicPath = imageManager.UploadGeneratedProfilePicInRoot(filledForm.ProfilePic);
-				}
-				else
-				{
-					user.ProfilePicPath = imageManager.UploadOriginalProfilePicInRoot(filledForm.ProfilePic);
-				}
-				userService.CreateUser(user);
-				//TODO Redirect to Email validation page then Successfull page
-				return RedirectToAction("Index", "Home");
+                if (filledForm.ProfilePic is null)
+                {
+                    filledForm.ProfilePic = imageManager.GeneratePlaceholderAvatar(filledForm.FirstName, filledForm.LastName);
+                    user.ProfilePicPath = imageManager.UploadGeneratedProfilePicInRoot(filledForm.ProfilePic);
+                }
+                else
+                {
+                    user.ProfilePicPath = imageManager.UploadOriginalProfilePicInRoot(filledForm.ProfilePic);
+                }
+                userService.CreateUser(user);
+                //TODO Redirect to Email validation page then Successfull page
+                return RedirectToAction("Index", "Home");
 
-			}
-			catch (EmailAlreadyExistException e)
-			{
-				this.Response.StatusCode = StatusCodes.Status409Conflict;
-				this.ViewData["ErrorMessage"] = e.Message;
-				return View(filledForm);
-			}
-			catch (UsernameAlreadyExistException e)
-			{
-				this.Response.StatusCode = StatusCodes.Status409Conflict;
-				this.ViewData["ErrorMessage"] = e.Message;
-				return View(filledForm);
-			}
-			catch (PhoneNumberAlreadyExistException e)
-			{
-				this.Response.StatusCode = StatusCodes.Status409Conflict;
-				this.ViewData["ErrorMessage"] = e.Message;
-				return View(filledForm);
-			}
-			catch (Exception e)
-			{
-				this.Response.StatusCode = StatusCodes.Status500InternalServerError;
-				this.ViewData["ErrorMessage"] = e.Message;
-				return View("Error");
-			}
-		}
+            }
+            catch (EmailAlreadyExistException e)
+            {
+                this.Response.StatusCode = StatusCodes.Status409Conflict;
+                this.ViewData["ErrorMessage"] = e.Message;
+                return View(filledForm);
+            }
+            catch (UsernameAlreadyExistException e)
+            {
+                this.Response.StatusCode = StatusCodes.Status409Conflict;
+                this.ViewData["ErrorMessage"] = e.Message;
+                return View(filledForm);
+            }
+            catch (PhoneNumberAlreadyExistException e)
+            {
+                this.Response.StatusCode = StatusCodes.Status409Conflict;
+                this.ViewData["ErrorMessage"] = e.Message;
+                return View(filledForm);
+            }
+            catch (Exception e)
+            {
+                this.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                this.ViewData["ErrorMessage"] = e.Message;
+                return View("Error");
+            }
+        }
 
-		[HttpGet]
-		public IActionResult EditUser([FromRoute] int id)
-		{
-			try
-			{
+        [HttpGet]
+        public IActionResult EditUser([FromRoute] int id)
+        {
+            try
+            {
 
-				if (!authManagerMVC.isLogged("LoggedUser"))
-				{
-					return RedirectToAction("Login", "User");
-				}
-				if (!authManagerMVC.isAdmin("roleId") && !authManagerMVC.isContentCreator("userId", id))
-				{
-					this.HttpContext.Response.StatusCode = StatusCodes.Status403Forbidden;
-					this.ViewData["ErrorMessage"] = AuthManagerMVC.notAthorized;
-					return View("Error");
-				}
-				if (id == 0)
-				{
-					this.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
-					this.ViewData["ErrorMessage"] = "Please provide ID for User!";
-					return View("Error");
-				}
-				var editUser = new EditUser();
-				var user = userService.GetUserById(id);
-				editUser.OldProfilePicPath = user.ProfilePicPath;
-				return View(editUser);
+                if (!authManagerMVC.isLogged("LoggedUser"))
+                {
+                    return RedirectToAction("Login", "User");
+                }
+                if (!authManagerMVC.isAdmin("roleId") && !authManagerMVC.isContentCreator("userId", id))
+                {
+                    this.HttpContext.Response.StatusCode = StatusCodes.Status403Forbidden;
+                    this.ViewData["ErrorMessage"] = AuthManagerMVC.notAthorized;
+                    return View("Error");
+                }
+                if (id == 0)
+                {
+                    this.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+                    this.ViewData["ErrorMessage"] = "Please provide ID for User!";
+                    return View("Error");
+                }
+                var editUser = new EditUser();
+                var user = userService.GetUserById(id);
+                editUser.OldProfilePicPath = user.ProfilePicPath;
+                return View(editUser);
 
-			}
-			catch (EntityNotFoundException e)
-			{
-				this.Response.StatusCode = StatusCodes.Status404NotFound;
-				this.ViewData["ErrorMessage"] = e.Message;
-				return View("Error");
-			}
-			catch (Exception e)
-			{
-				this.Response.StatusCode = StatusCodes.Status500InternalServerError;
-				this.ViewData["ErrorMessage"] = e.Message;
-				return View("Error");
-			}
-		}
+            }
+            catch (EntityNotFoundException e)
+            {
+                this.Response.StatusCode = StatusCodes.Status404NotFound;
+                this.ViewData["ErrorMessage"] = e.Message;
+                return View("Error");
+            }
+            catch (Exception e)
+            {
+                this.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                this.ViewData["ErrorMessage"] = e.Message;
+                return View("Error");
+            }
+        }
 
-		[HttpPost]
-		public IActionResult EditUser([FromRoute] int id, EditUser editedUser)
-		{
-			try
-			{
-				if (!authManagerMVC.isLogged("LoggedUser"))
-				{
-					return RedirectToAction("Login", "User");
-				}
-				if (!authManagerMVC.isAdmin("roleId") && !authManagerMVC.isContentCreator("userId", id))
-				{
-					this.HttpContext.Response.StatusCode = StatusCodes.Status403Forbidden;
-					this.ViewData["ErrorMessage"] = AuthManagerMVC.notAthorized;
-					return View("Error");
-				}
-				if (!this.ModelState.IsValid)
-				{
-					return View(editedUser);
-				}
-				if (editedUser.FirstName is null &&
-					editedUser.LastName is null &&
-					editedUser.Password is null &&
-					editedUser.Email is null &&
-					editedUser.PhoneNumber is null &&
-					editedUser.ProfilePic is null)
-				{
-					this.ViewData["ErrorMessage"] = "There's nothing filled!";
-					return View(editedUser);
-				}
-				var newUserValues = mapper.Map<User>(editedUser);
+        [HttpPost]
+        public IActionResult EditUser([FromRoute] int id, EditUser editedUser)
+        {
+            try
+            {
+                if (!authManagerMVC.isLogged("LoggedUser"))
+                {
+                    return RedirectToAction("Login", "User");
+                }
+                if (!authManagerMVC.isAdmin("roleId") && !authManagerMVC.isContentCreator("userId", id))
+                {
+                    this.HttpContext.Response.StatusCode = StatusCodes.Status403Forbidden;
+                    this.ViewData["ErrorMessage"] = AuthManagerMVC.notAthorized;
+                    return View("Error");
+                }
+                if (!this.ModelState.IsValid)
+                {
+                    return View(editedUser);
+                }
+                if (editedUser.FirstName is null &&
+                    editedUser.LastName is null &&
+                    editedUser.Password is null &&
+                    editedUser.Email is null &&
+                    editedUser.PhoneNumber is null &&
+                    editedUser.ProfilePic is null)
+                {
+                    this.ViewData["ErrorMessage"] = "There's nothing filled!";
+                    return View(editedUser);
+                }
+                var newUserValues = mapper.Map<User>(editedUser);
 
-				if (editedUser.ProfilePic is not null)
-				{
-					newUserValues.ProfilePicPath = imageManager.UploadOriginalProfilePicInRoot(editedUser.ProfilePic);
-					imageManager.DeleteProfilePicFromRoot(editedUser.OldProfilePicPath);
-				}
-				userService.UpdateUser(id, newUserValues);
-				//TODO Redirect to Successfull page
-				return RedirectToAction("Index", "Home");
+                if (editedUser.ProfilePic is not null)
+                {
+                    newUserValues.ProfilePicPath = imageManager.UploadOriginalProfilePicInRoot(editedUser.ProfilePic);
+                    imageManager.DeleteProfilePicFromRoot(editedUser.OldProfilePicPath);
+                }
+                userService.UpdateUser(id, newUserValues);
+                //TODO Redirect to Successfull page
+                return RedirectToAction("Index", "Home");
 
-			}
-			catch (EmailAlreadyExistException e)
-			{
-				this.Response.StatusCode = StatusCodes.Status409Conflict;
-				this.ViewData["ErrorMessage"] = e.Message;
-				return View("Error");
-			}
-			catch (PhoneNumberAlreadyExistException e)
-			{
-				this.Response.StatusCode = StatusCodes.Status409Conflict;
-				this.ViewData["ErrorMessage"] = e.Message;
-				return View("Error");
-			}
-			catch (Exception e)
-			{
-				this.Response.StatusCode = StatusCodes.Status500InternalServerError;
-				this.ViewData["ErrorMessage"] = e.Message;
-				return View("Error");
-			}
-		}
-	}
+            }
+            catch (EmailAlreadyExistException e)
+            {
+                this.Response.StatusCode = StatusCodes.Status409Conflict;
+                this.ViewData["ErrorMessage"] = e.Message;
+                return View("Error");
+            }
+            catch (PhoneNumberAlreadyExistException e)
+            {
+                this.Response.StatusCode = StatusCodes.Status409Conflict;
+                this.ViewData["ErrorMessage"] = e.Message;
+                return View("Error");
+            }
+            catch (Exception e)
+            {
+                this.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                this.ViewData["ErrorMessage"] = e.Message;
+                return View("Error");
+            }
+        }
+    }
 }
