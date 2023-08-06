3e09971ce547561831740410968bf29f147b5a1b
Nikolai Gigov <nikolaivgigov2@gmail.com>
2023-08-06 13:39:13 +0300

    -Made finalize exchange form a bit more readable -Label text changes

---
+++ VirtualWallet/VirtualWalletWeb/ViewControllers/TransactionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VirtualWallet.Web.ViewControllers
+{
+	public class TransactionController : Controller
+	{
+		public IActionResult CreateTransfer()
+		{
+			return View();
+		}
+	}
+}
