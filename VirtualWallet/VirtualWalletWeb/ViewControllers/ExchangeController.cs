4b56b5cba942effdd9dfb2b8ec506d632d2c42f5
Atanas Iliev <atanasiliev1293@gmail.com>
2023-08-06 14:36:40 +0300

    Added Expected Rate and Expected amount to Confirm Exchange View

--- VirtualWallet/VirtualWalletWeb/ViewControllers/ExchangeController.cs
+++ VirtualWallet/VirtualWalletWeb/ViewControllers/ExchangeController.cs
@@ -12,127 +12,137 @@ using VirtualWallet.Web.Helper.Contracts;
 
 namespace VirtualWallet.Web.ViewControllers
 {
-    public class ExchangeController : Controller
-    {
-        private readonly IAuthManagerMVC authManagerMVC;
-        private readonly ICurrencyService currencyService;
-        private readonly IMapper mapper;
-        private readonly IWalletService walletService;
+	public class ExchangeController : Controller
+	{
+		private readonly IAuthManagerMVC authManagerMVC;
+		private readonly ICurrencyService currencyService;
+		private readonly IMapper mapper;
+		private readonly IWalletService walletService;
+		private readonly IExchangeService exchangeService;
 
-        public ExchangeController(IAuthManagerMVC authManagerMVC, ICurrencyService currencyService, IMapper mapper, IWalletService walletService)
-        {
-            this.authManagerMVC = authManagerMVC;
-            this.currencyService = currencyService;
-            this.mapper = mapper;
-            this.walletService = walletService;
-        }
-        public IActionResult FinalizeExchangeDev()
-        {
-            return View("FinalizeExchange");
-        }
-        public IActionResult MakeExchange(CreateExcahngeDto createExchange)
-        {
-            try
-            {
-                if (!authManagerMVC.isLogged("LoggedUser"))
-                {
-                    return RedirectToAction("Login", "User");
-                }
-                this.ViewBag.AmountToEdit = createExchange.Amount;
-                ViewData["Currencies"] = LoadCurrencies();
-                return View(createExchange);
+		public ExchangeController(IAuthManagerMVC authManagerMVC,
+									ICurrencyService currencyService,
+									IMapper mapper,
+									IWalletService walletService,
+									IExchangeService exchangeService)
+		{
+			this.authManagerMVC = authManagerMVC;
+			this.currencyService = currencyService;
+			this.mapper = mapper;
+			this.walletService = walletService;
+			this.exchangeService = exchangeService;
+		}
+		public IActionResult ConfirmExchangeDev()
+		{
+			return View("ConfirmExchange");
+		}
+		public IActionResult MakeExchange(CreateExcahngeDto createExchange)
+		{
+			try
+			{
+				if (!authManagerMVC.isLogged("LoggedUser"))
+				{
+					return RedirectToAction("Login", "User");
+				}
+				this.ViewBag.AmountToEdit = createExchange.Amount;
+				ViewData["Currencies"] = LoadCurrencies();
+				return View(createExchange);
 
-            }
-            catch (Exception e)
-            {
-                this.Response.StatusCode = StatusCodes.Status500InternalServerError;
-                this.ViewData["ErrorMessage"] = e.Message;
-                return View("Error");
-            }
-        }
+			}
+			catch (Exception e)
+			{
+				this.Response.StatusCode = StatusCodes.Status500InternalServerError;
+				this.ViewData["ErrorMessage"] = e.Message;
+				return View("Error");
+			}
+		}
 
-        public IActionResult ConfirmExchange(CreateExcahngeDto createExchange)
-        {
-            try
-            {
-                if (!authManagerMVC.isLogged("LoggedUser"))
-                {
-                    return RedirectToAction("Login", "User");
-                }
+		public IActionResult ConfirmExchange(CreateExcahngeDto createExchange)
+		{
+			try
+			{
+				if (!authManagerMVC.isLogged("LoggedUser"))
+				{
+					return RedirectToAction("Login", "User");
+				}
 
-                var currencies = new List<CurrencyViewModel>();
-                //Влизаме тук ако някое от полетата не е попълнено или е подадена негативна или неадекватна стойност за Amount!
-                if (!this.ModelState.IsValid)
-                {
-                    currencies = currencyService.GetCurrencies().Select(c => mapper.Map<CurrencyViewModel>(c)).ToList();
-                    ViewData["Currencies"] = currencies;
-                    this.ViewData["ErrorMessage"] = (createExchange.Amount <= 0 ? "Please provide positive Amount!" : "Please provide input!");
-                    return View("MakeExchange", createExchange);
-                }
+				var currencies = new List<CurrencyViewModel>();
+				//Влизаме тук ако някое от полетата не е попълнено или е подадена негативна или неадекватна стойност за Amount!
+				if (!this.ModelState.IsValid)
+				{
+					currencies = currencyService.GetCurrencies().Select(c => mapper.Map<CurrencyViewModel>(c)).ToList();
+					ViewData["Currencies"] = currencies;
+					this.ViewData["ErrorMessage"] = (createExchange.Amount <= 0 ? "Please provide positive Amount!" : "Please provide input!");
+					return View("MakeExchange", createExchange);
+				}
 
-                var loggedUserId = this.HttpContext.Session.GetInt32("userId");
-                var loggedUserWallet = walletService.GetWalletById((int)loggedUserId, (int)loggedUserId);
+				var loggedUserId = this.HttpContext.Session.GetInt32("userId");
+				var loggedUserWallet = walletService.GetWalletById((int)loggedUserId, (int)loggedUserId);
 
-                _ = walletService.ValidateFunds(loggedUserWallet, createExchange);
+				_ = walletService.ValidateFunds(loggedUserWallet, createExchange);
+
+				ViewBag.Rate = exchangeService.GetExchangeRate(createExchange.From, createExchange.To).Result;
+				ViewBag.ExpectedAmount = ViewBag.Rate * createExchange.Amount;
 
 
-                return View(createExchange);
 
-            }
-            catch (InsufficientFundsException e)
-            {
-                this.ViewData["ErrorMessage"] = e.Message;
-                ViewData["Currencies"] = LoadCurrencies();
-                return View("MakeExchange", createExchange);
-            }
-            catch (EntityNotFoundException e)
-            {
-                this.ViewData["ErrorMessage"] = e.Message;
-                ViewData["Currencies"] = LoadCurrencies();
-                return View("MakeExchange", createExchange);
-            }
-            catch (ArgumentException e)
-            {
-                this.ViewData["ErrorMessage"] = e.Message;
-                ViewData["Currencies"] = LoadCurrencies();
-                return View("MakeExchange", createExchange);
-            }
-            catch (Exception e)
-            {
-                this.Response.StatusCode = StatusCodes.Status500InternalServerError;
-                this.ViewData["ErrorMessage"] = e.Message;
-                return View("Error");
-            }
-        }
-        public IActionResult FinalizeExchange(CreateExcahngeDto createExchange)
-        {
-            try
-            {
-                var loggedUserId = this.HttpContext.Session.GetInt32("userId");
-                var exchange = walletService.ExchangeFunds(createExchange, (int)loggedUserId, (int)loggedUserId).Result;
-                var exchangeDto = mapper.Map<GetExchangeDto>(exchange);
-                return View(exchangeDto);
+				return View(createExchange);
 
-            }
+			}
+			catch (InsufficientFundsException e)
+			{
+				this.ViewData["ErrorMessage"] = e.Message;
+				ViewData["Currencies"] = LoadCurrencies();
+				return View("MakeExchange", createExchange);
+			}
+			catch (EntityNotFoundException e)
+			{
+				this.ViewData["ErrorMessage"] = e.Message;
+				ViewData["Currencies"] = LoadCurrencies();
+				return View("MakeExchange", createExchange);
+			}
+			catch (ArgumentException e)
+			{
+				this.ViewData["ErrorMessage"] = e.Message;
+				ViewData["Currencies"] = LoadCurrencies();
+				return View("MakeExchange", createExchange);
+			}
+			catch (Exception e)
+			{
+				this.Response.StatusCode = StatusCodes.Status500InternalServerError;
+				this.ViewData["ErrorMessage"] = e.Message;
+				return View("Error");
+			}
+		}
+		public IActionResult FinalizeExchange(CreateExcahngeDto createExchange)
+		{
+			try
+			{
+				var loggedUserId = this.HttpContext.Session.GetInt32("userId");
+				var exchange = walletService.ExchangeFunds(createExchange, (int)loggedUserId, (int)loggedUserId).Result;
+				var exchangeDto = mapper.Map<GetExchangeDto>(exchange);
+				return View(exchangeDto);
 
-            catch (ArgumentException e)
-            {
-                this.ViewData["ErrorMessage"] = e.Message;
-                ViewData["Currencies"] = LoadCurrencies();
-                return View("MakeExchange", createExchange);
-            }
-            catch (Exception e)
-            {
-                this.Response.StatusCode = StatusCodes.Status500InternalServerError;
-                this.ViewData["ErrorMessage"] = e.Message;
-                return View("Error");
-            }
+			}
 
-        }
+			catch (ArgumentException e)
+			{
+				this.ViewData["ErrorMessage"] = e.Message;
+				ViewData["Currencies"] = LoadCurrencies();
+				return View("MakeExchange", createExchange);
+			}
+			catch (Exception e)
+			{
+				this.Response.StatusCode = StatusCodes.Status500InternalServerError;
+				this.ViewData["ErrorMessage"] = e.Message;
+				return View("Error");
+			}
 
-        private List<CurrencyViewModel> LoadCurrencies()
-        {
-            return currencyService.GetCurrencies().Select(c => mapper.Map<CurrencyViewModel>(c)).ToList();
-        }
-    }
+		}
+
+		private List<CurrencyViewModel> LoadCurrencies()
+		{
+			return currencyService.GetCurrencies().Select(c => mapper.Map<CurrencyViewModel>(c)).ToList();
+		}
+	}
 }
