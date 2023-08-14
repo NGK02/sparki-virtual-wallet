f818d2ca8fe9a8766cc01211947e60c40e717c26
Atanas Iliev <atanasiliev1293@gmail.com>
2023-08-14 13:30:30 +0300

    Make pagination in Dashboard/Exchange to save values when paginate

--- VirtualWallet/VirtualWallet.DataAccess/Repositories/ExchangeRepository.cs
+++ VirtualWallet/VirtualWallet.DataAccess/Repositories/ExchangeRepository.cs
@@ -9,33 +9,65 @@ using VirtualWallet.DataAccess.Repositories.Contracts;
 
 namespace VirtualWallet.DataAccess.Repositories
 {
-	public class ExchangeRepository:IExchangeRepository
-	{
-		private readonly WalletDbContext database;
+    public class ExchangeRepository : IExchangeRepository
+    {
+        private readonly WalletDbContext database;
 
-		public ExchangeRepository(WalletDbContext database)
-		{
-			this.database = database;
-		}
-		public bool AddExchange(Exchange exchange)
-		{
-			exchange.CreatedOn = DateTime.Now;
-			database.Exchanges.Add(exchange);
-			database.SaveChanges();
-			return true;
-		}
+        public ExchangeRepository(WalletDbContext database)
+        {
+            this.database = database;
+        }
+        public bool AddExchange(Exchange exchange)
+        {
+            exchange.CreatedOn = DateTime.Now;
+            database.Exchanges.Add(exchange);
+            database.SaveChanges();
+            return true;
+        }
 
-		public IEnumerable<Exchange> GetUserExchanges(int walletId, VirtualWallet.DataAccess.QueryParameters.QueryParameters parameters)
-		{
-			return GetQueryableExchanges().Where(t => t.WalletId == walletId).ToList();
-		}
+        public IEnumerable<Exchange> GetUserExchanges(int walletId, VirtualWallet.DataAccess.QueryParameters.QueryParameters parameters)
+        {
+            var exchangesQuerable = GetQueryableExchanges().Where(t => t.WalletId == walletId);
+            return SortExchangesBy(exchangesQuerable, parameters).ToList();
 
-		private IQueryable<Exchange> GetQueryableExchanges()
-		{
-			return database.Exchanges
-				.Where(e => !e.IsDeleted)
-				.Include(e => e.FromCurrency)
-				.Include(e => e.Wallet);
-		}
-	}
+        }
+
+        private IQueryable<Exchange> SortExchangesBy(IQueryable<Exchange> exchangesQuerable, QueryParameters.QueryParameters parameters)
+        {
+            if (parameters.SortBy is not null)
+            {
+
+                if (parameters.SortBy.Equals("Date", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    exchangesQuerable = exchangesQuerable.OrderBy(e => e.CreatedOn);
+                }
+                if (parameters.SortBy.Equals("Amount", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    exchangesQuerable = exchangesQuerable.OrderBy(e => e.Amount);
+                }
+                if (parameters.SortBy.Equals("ExchangedAmount", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    exchangesQuerable = exchangesQuerable.OrderBy(e => e.ExchangedAmout);
+                }
+
+            }
+
+            if (parameters.SortOrder is not null)
+            {
+                if (parameters.SortOrder.Equals("Descending", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    exchangesQuerable = exchangesQuerable.Reverse();
+                }
+            }
+            return exchangesQuerable;
+        }
+
+        private IQueryable<Exchange> GetQueryableExchanges()
+        {
+            return database.Exchanges
+                .Where(e => !e.IsDeleted)
+                .Include(e => e.FromCurrency)
+                .Include(e => e.Wallet);
+        }
+    }
 }
