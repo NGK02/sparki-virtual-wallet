4ca27c47d3f8d7450cb80985724fe2b8a6ebc398
katrinlilova <katrin.lilova@proton.me>
2023-08-17 12:58:54 +0300

    Refactor WalletTransactionService and write summaries for IWalletTransactionService

--- VirtualWallet/VirtualWalletServices/Services/Contracts/IWalletTransactionService.cs
+++ VirtualWallet/VirtualWalletServices/Services/Contracts/IWalletTransactionService.cs
@@ -1,24 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using VirtualWallet.DataAccess.Enums;
+﻿using VirtualWallet.DataAccess.Enums;
 using VirtualWallet.DataAccess.Models;
 using VirtualWallet.DataAccess.QueryParameters;
 
 namespace VirtualWallet.Business.Services.Contracts
 {
-	public interface IWalletTransactionService
+    public interface IWalletTransactionService
 	{
-		int GetTransactionsCount();
-		WalletTransaction CreateTransaction(WalletTransaction walletTransaction, User sender);
-		WalletTransaction CreateTransaction(WalletTransaction walletTransaction);
+        Dictionary<string, decimal> GetUserIncomingTransactionsForLastWeek(int userId, CurrencyCode toCurrency);
+
+        Dictionary<string, decimal> GetUserOutgoingTransactionsForLastWeek(int userId, CurrencyCode toCurrency);
+
+        int GetTransactionsCount();
+
+        List<WalletTransaction> GetUserWalletTransactions(WalletTransactionQueryParameters queryParameters, int userId);
+
+        List<WalletTransaction> GetWalletTransactions(WalletTransactionQueryParameters queryParameters);
+
+        WalletTransaction CreateTransaction(WalletTransaction walletTransaction);
+
+        WalletTransaction CreateTransaction(WalletTransaction walletTransaction, User sender);
 
 		WalletTransaction GetWalletTransactionById(int id, string username);
-		List<WalletTransaction> GetUserWalletTransactions(WalletTransactionQueryParameters queryParameters, int userId);
-		List<WalletTransaction> GetWalletTransactions(WalletTransactionQueryParameters queryParameters);
-		Dictionary<string, decimal> GetUserOutgoingTransactionsForLastWeek(int userId, CurrencyCode toCurrency);
-        Dictionary<string, decimal> GetUserIncomingTransactionsForLastWeek(int userId, CurrencyCode toCurrency);
     }
 }
