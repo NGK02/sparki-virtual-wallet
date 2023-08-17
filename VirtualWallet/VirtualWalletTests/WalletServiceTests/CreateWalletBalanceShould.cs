40e67e94b8dde8dfb9533821eb2f8cbd71fd1915
katrinlilova <katrin.lilova@proton.me>
2023-08-18 01:23:35 +0300

    Write unit tests for WalletService

---
+++ VirtualWallet/VirtualWalletTests/WalletServiceTests/CreateWalletBalanceShould.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using VirtualWallet.Business.Services;
+using VirtualWallet.DataAccess.Models;
+using VirtualWallet.DataAccess.Repositories.Contracts;
+
+namespace VirtualWalletTests.WalletServiceTests
+{
+    [TestClass]
+    public class CreateWalletBalanceShould
+    {
+        [TestMethod]
+        public void CreateWalletBalanceShould_CreateBalanceSuccessfully()
+        {
+            var currencyId = 1;
+            var walletId = 2;
+
+            var walletRepositoryMock = new Mock<IWalletRepository>();
+            walletRepositoryMock.Setup(repo => repo.CreateWalletBalance(currencyId, walletId))
+                               .Returns(new Balance { CurrencyId = currencyId, WalletId = walletId })
+                               .Verifiable();
+
+            var walletService = new WalletService(null, null, null, walletRepositoryMock.Object);
+
+            var createdBalance = walletService.CreateWalletBalance(currencyId, walletId);
+
+            Assert.IsNotNull(createdBalance);
+            Assert.AreEqual(currencyId, createdBalance.CurrencyId);
+            Assert.AreEqual(walletId, createdBalance.WalletId);
+
+            walletRepositoryMock.Verify(repo => repo.CreateWalletBalance(currencyId, walletId), Times.Once);
+        }
+    }
+}
---
+++ VirtualWallet/VirtualWalletTests/WalletServiceTests/DistributeFundsForReferralsShould.cs
@@ -0,0 +1,42 @@
+﻿using Moq;
+using VirtualWallet.Business.Services.Contracts;
+using VirtualWallet.Business.Services;
+using VirtualWallet.DataAccess.Models;
+using VirtualWallet.DataAccess.Repositories.Contracts;
+
+namespace VirtualWalletTests.WalletServiceTests
+{
+    [TestClass]
+    public class DistributeFundsForReferralsShould
+    {
+        [TestMethod]
+        public void DistributeFundsForReferralsShould_DistributeFundsSuccessfully()
+        {
+            var referrerId = 1;
+            var referredUserId = 2;
+            var amount = 100.0m;
+            var currencyId = 1;
+
+            var referrer = new User { Id = referrerId, Wallet = new Wallet() };
+            var referredUser = new User { Id = referredUserId };
+            var referrerBalance = new Balance { CurrencyId = currencyId };
+            var referredUserBalance = new Balance { CurrencyId = currencyId };
+
+            var userServiceMock = new Mock<IUserService>();
+            userServiceMock.Setup(mock => mock.GetUserById(referrerId)).Returns(referrer);
+            userServiceMock.Setup(mock => mock.GetUserById(referredUserId)).Returns(referredUser);
+
+            var walletRepositoryMock = new Mock<IWalletRepository>();
+            walletRepositoryMock.Setup(mock => mock.CreateWalletBalance(currencyId, referrerId)).Returns(referrerBalance);
+            walletRepositoryMock.Setup(mock => mock.CreateWalletBalance(currencyId, referredUserId)).Returns(referredUserBalance);
+
+            var walletService = new WalletService(null, null, userServiceMock.Object, walletRepositoryMock.Object);
+
+            walletService.DistributeFundsForReferrals(referrerId, referredUserId, amount, currencyId);
+
+            Assert.AreEqual(amount, referrerBalance.Amount);
+            Assert.AreEqual(amount, referredUserBalance.Amount);
+            walletRepositoryMock.Verify(mock => mock.DistributeFundsForReferrals(referrerBalance, referredUserBalance), Times.Once);
+        }
+    }
+}
