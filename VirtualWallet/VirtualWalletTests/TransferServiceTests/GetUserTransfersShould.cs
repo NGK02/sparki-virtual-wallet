e2f3bbbd0a7d7c022a88d29730c59961fd544d12
katrinlilova <katrin.lilova@proton.me>
2023-08-17 23:52:39 +0300

    Finish unit testing TransferService

---
+++ VirtualWallet/VirtualWalletTests/TransferServiceTests/GetUserTransfersShould.cs
@@ -0,0 +1,98 @@
+﻿using Moq;
+using VirtualWallet.Business.AuthManager;
+using VirtualWallet.Business.Exceptions;
+using VirtualWallet.Business.Services;
+using VirtualWallet.Business.Services.Contracts;
+using VirtualWallet.DataAccess.Models;
+using VirtualWallet.DataAccess.QueryParameters;
+using VirtualWallet.DataAccess.Repositories.Contracts;
+
+namespace VirtualWalletTests.TransferServiceTests
+{
+    [TestClass]
+    public class GetUserTransfersShould
+    {
+        private Mock<IUserService> userServiceMock;
+        private Mock<IAuthManager> authManagerMock;
+        private Mock<ITransferRepository> transferRepositoryMock;
+        private TransferService transferService;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            userServiceMock = new Mock<IUserService>();
+            authManagerMock = new Mock<IAuthManager>();
+            transferRepositoryMock = new Mock<ITransferRepository>();
+
+            transferService = new TransferService(
+                authManager: authManagerMock.Object,
+                currencyService: null,
+                transferRepository: transferRepositoryMock.Object,
+                userService: userServiceMock.Object,
+                walletService: null
+            );
+        }
+
+        [TestMethod]
+        public void GetUserTransfers_Should_ReturnTransfers_WhenTransfersExist()
+        {
+            var userId = 1;
+            var transfers = new List<Transfer>
+            {
+                new Transfer { Id = 1 },
+                new Transfer { Id = 2 }
+            };
+
+            transferRepositoryMock.Setup(repo => repo.GetUserTransfers(userId)).Returns(transfers);
+
+            var result = transferService.GetUserTransfers(userId).ToList();
+
+            transferRepositoryMock.Verify();
+
+            CollectionAssert.AreEqual(transfers, result);
+        }
+
+        [TestMethod]
+        public void GetUserTransfers_Should_ThrowEntityNotFoundException_WhenNoTransfersAvailable()
+        {
+            var userId = 1;
+            var emptyTransfers = new List<Transfer>();
+
+            transferRepositoryMock.Setup(repo => repo.GetUserTransfers(userId)).Returns(emptyTransfers);
+
+            Assert.ThrowsException<EntityNotFoundException>(() => transferService.GetUserTransfers(userId));
+        }
+
+        [TestMethod]
+        public void GetUserTransfersWithParams_Should_ReturnTransfers_WhenTransfersExist()
+        {
+            var userId = 1;
+            var parameters = new QueryParams();
+            var transfers = new List<Transfer>
+            {
+                new Transfer { Id = 1 },
+                new Transfer { Id = 2 }
+            };
+
+            transferRepositoryMock.Setup(repo => repo.GetUserTransfers(userId, parameters)).Returns(transfers);
+
+            var result = transferService.GetUserTransfers(userId, parameters).ToList();
+
+            transferRepositoryMock.Verify();
+
+            CollectionAssert.AreEqual(transfers, result);
+        }
+
+        [TestMethod]
+        public void GetUserTransfersWithParams_Should_ThrowEntityNotFoundException_WhenNoTransfersAvailable()
+        {
+            var userId = 1;
+            var parameters = new QueryParams();
+            var emptyTransfers = new List<Transfer>();
+
+            transferRepositoryMock.Setup(repo => repo.GetUserTransfers(userId, parameters)).Returns(emptyTransfers);
+
+            Assert.ThrowsException<EntityNotFoundException>(() => transferService.GetUserTransfers(userId, parameters));
+        }
+    }
+}
---
+++ VirtualWallet/VirtualWalletTests/TransferServiceTests/TransferFromWalletShould.cs
@@ -0,0 +1,94 @@
+﻿using Moq;
+using VirtualWallet.Business.Exceptions;
+using VirtualWallet.Business.Services;
+using VirtualWallet.Business.Services.Contracts;
+using VirtualWallet.DataAccess.Models;
+using VirtualWallet.DataAccess.Repositories.Contracts;
+
+namespace VirtualWalletTests.TransferServiceTests
+{
+    [TestClass]
+    public class TransferFromWalletShould
+    {
+        private Mock<ICurrencyService> currencyServiceMock;
+        private Mock<ITransferRepository> transferRepositoryMock;
+        private Mock<IWalletService> walletServiceMock;
+        private TransferService transferService;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            currencyServiceMock = new Mock<ICurrencyService>();
+            transferRepositoryMock = new Mock<ITransferRepository>();
+            walletServiceMock = new Mock<IWalletService>();
+
+            transferService = new TransferService(null, currencyServiceMock.Object, transferRepositoryMock.Object, null, walletServiceMock.Object);
+        }
+
+        [TestMethod]
+        public void CreateTransfer_InsufficientBalance_ThrowsInsufficientFundsException()
+        {
+            decimal transferAmount = 1000;
+            var currencyId = 1;
+            var walletId = 123;
+
+            var wallet = new Wallet { Id = walletId, Balances = new List<Balance>() };
+            walletServiceMock.Setup(service => service.GetWalletById(walletId)).Returns(wallet);
+
+            var currency = new Currency { Id = currencyId };
+            currencyServiceMock.Setup(service => service.GetCurrencyById(currencyId)).Returns(currency);
+
+            var balance = new Balance { CurrencyId = currencyId, Amount = 500, Currency = currency };
+            wallet.Balances.Add(balance);
+
+            var transfer = new Transfer { WalletId = walletId, CurrencyId = currencyId, Amount = transferAmount };
+
+            Assert.ThrowsException<InsufficientFundsException>(() => transferService.CreateTransfer(transfer));
+        }
+
+        [TestMethod]
+        public void CreateTransfer_NullBalance_ThrowsInsufficientFundsException()
+        {
+            var walletId = 123;
+            var currencyId = 1;
+            decimal transferAmount = 1000;
+
+            var wallet = new Wallet { Id = walletId, Balances = new List<Balance>() };
+
+            wallet.Balances.Add(new Balance { CurrencyId = 2 });
+
+            walletServiceMock.Setup(service => service.GetWalletById(walletId)).Returns(wallet);
+
+            var currency = new Currency { Id = currencyId };
+            currencyServiceMock.Setup(service => service.GetCurrencyById(currencyId)).Returns(currency);
+
+            var transfer = new Transfer { WalletId = walletId, CurrencyId = currencyId, Amount = transferAmount };
+
+            Assert.ThrowsException<InsufficientFundsException>(() => transferService.CreateTransfer(transfer));
+        }
+
+        [TestMethod]
+        public void CreateTransfer_TransferFromWallet_SuccessfulTransfer()
+        {
+            var walletId = 123;
+            var currencyId = 1;
+            var initialBalanceAmount = 100;
+
+            var wallet = new Wallet { Id = walletId, Balances = new List<Balance>() };
+            walletServiceMock.Setup(service => service.GetWalletById(walletId)).Returns(wallet);
+
+            var currency = new Currency { Id = currencyId };
+            currencyServiceMock.Setup(service => service.GetCurrencyById(currencyId)).Returns(currency);
+
+            var balance = new Balance { CurrencyId = currencyId, Amount = initialBalanceAmount, Currency = currency };
+            wallet.Balances.Add(balance);
+
+            var transfer = new Transfer { WalletId = wallet.Id, CurrencyId = balance.CurrencyId, Amount = 50 };
+
+            transferService.CreateTransfer(transfer);
+            transferRepositoryMock.Verify(repo => repo.AddTransfer(transfer), Times.Once);
+
+            Assert.AreEqual(initialBalanceAmount - transfer.Amount, balance.Amount);
+        }
+    }
+}
